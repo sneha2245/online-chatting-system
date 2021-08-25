22ec8c40e10d94d1fbde1ec6d3c0be89224ae309
Sneha Paul <32849265+sneha2245@users.noreply.github.com>
2021-08-25 17:14:48 +0530

    Add files via upload

---
+++ groupchat.php
@@ -0,0 +1,23 @@
+<div class="col-md-5 col-sm-12">
+	<div class="row">										
+		<div class="col-md-12 col-sm-12">
+			<div class="form-control">
+				<div style="border-bottom:1px solid black"><h3>Group Chat</h3></div>
+				<div class="row">										
+					<div class="col-md-12 col-sm-12">
+						<div class="form-group" style="margin-top: 15px;">
+							<input type="text" placeholder="Enter Your Group Name" name="group_name" class="form-control"/>
+						</div>
+					</div>
+				</div>	
+				<div class="row">										
+					<div class="col-md-12 col-sm-12">
+						<div class="scrollable" style="height:455px;">
+							
+						</div>
+					</div>
+				</div>				
+			</div>                                      
+		</div>
+	</div>
+</div>	
