19c0bbc6a859d9b45c9d588c1cb11f0e79ed884f
Sneha Paul <32849265+sneha2245@users.noreply.github.com>
2021-08-25 17:14:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<meta charset="utf-8">
+	<title>Chatting System - Registrations</title>
+	<meta name="description" content="">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/new_style.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">		
+</head>
+<body class="instructor-home">
+	<div class="rs-events sec-spacer sec-color pb-70 pt-70">
+		<div class="container" style="background-color: rgb(237,237,237);border-radius: .3rem;"> 			
+			<div class="row">
+                <div class="col-md-12 offset-md-1">
+                    <div class="contact-comment-section">
+                        <div class="row" style="margin-bottom: 30px;">
+							<!-- LOG IN -->
+							<?php include'signin.php'?>							
+							<!-- CREAT Account -->
+							<?php include'createaccount.php' ?>
+						</div>
+					</div>
+                </div>
+            </div>
+		</div>
+	</div>	
+</body>
+</html>
